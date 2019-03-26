da84c7086bae05ec543b6db9e3ec38aa64bc1ff5
Naveen Tharalla <naveen_tharalla@yahoo.com>
2019-03-26 08:25:45 -0400

    enable Junit test execution

--- config/build_development.sh
+++ config/build_development.sh
@@ -3,7 +3,7 @@ mkdir /tmp/artifacts
 echo "> Build StormWaterCalculator server and ui .war" 
 echo "> Change directory to swcalculator-server , generate server .war fi le"
 cd ../swcalculator-server 
-mvn clean install -DskipTests
+mvn clean install 
 mvn sonar:sonar -Dsonar.host.url=$SONARQUBE_URL  -Dsonar.login=$SONARQUBE_TOKEN -Dsonar.projectKey=SWC-Server -Dsonar.projectName=SWC-Server
 cp target/swcalculator-server.war /tmp/artifacts
 echo "> Change directory to FrontEnd and create UI .war file"

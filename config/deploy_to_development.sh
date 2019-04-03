16b0c5749710ba2720fcdd1cfe9e472cd15f8cee
Naveen Tharalla <naveen_tharalla@yahoo.com>
2019-04-03 14:32:17 -0400

    Update deploy_to_development.sh
    undeploy the application before deploy

--- config/deploy_to_development.sh
+++ config/deploy_to_development.sh
@@ -1,5 +1,7 @@
 cd ..
 echo "> Deploy to swcweb.erg.com"
+curl -u $username:$password "http://swcweb.erg.com/manager/text/undeploy?path=/swcalculator-server"
 curl -u $username:$password --upload-file swcalculator-server/target/swcalculator-server.war "http://swcweb.erg.com/manager/text/deploy?path=/swcalculator-server&update=true"
 echo "> Deploy stormwatercalculator.war"
+curl -u $username:$password "http://swcweb.erg.com/manager/text/undeploy?path=/stormwatercalculator"
 curl -u $username:$password --upload-file FrontEnd/target/stormwatercalculator.war "http://swcweb.erg.com/manager/text/deploy?path=/stormwatercalculator&update=true" 
